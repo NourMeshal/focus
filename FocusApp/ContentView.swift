c5d95539d053cdcaac9b50d54cd377010c855e5e
NourMeshal <nourmeshal@outlook.com>
2021-04-30 00:15:17 +0300

    Initial Commit

---
+++ FocusApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FocusApp
+//
+//  Created by Nour Altunaib on 4/30/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FocusApp/FocusAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusAppApp.swift
+//  FocusApp
+//
+//  Created by Nour Altunaib on 4/30/21.
+//
+
+import SwiftUI
+
+@main
+struct FocusAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
